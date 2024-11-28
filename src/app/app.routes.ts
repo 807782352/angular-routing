6815e779106bc13b7cf3c3996cfe51cde8d92741
807782352 <kyrie807782352@gmail.com>
2024-11-28 12:07:54 -0500

    add RouterLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
 
 export const routes: Routes = [
+  { path: '', component: NoTaskComponent }, // <your-domain>/
   { path: 'tasks', component: TasksComponent }, // <your-domain>/tasks
 ];
