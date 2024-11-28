e1342203cb4f061d4c88757f05e9c057c3d88cc8
807782352 <kyrie807782352@gmail.com>
2024-11-28 12:38:18 -0500

    route with dynamic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
   { path: '', component: NoTaskComponent }, // <your-domain>/
-  { path: 'tasks', component: TasksComponent }, // <your-domain>/tasks
+
+  // { path: 'tasks', component: TasksComponent }, // <your-domain>/tasks
+
+  {path: 'users/:userId', component: UserTasksComponent}   
 ];
